55c28da48ad96102fb409d19b0b1f7d38bf80b97
Lachkar El hassan <99615141+lachkar98@users.noreply.github.com>
2023-03-29 11:51:06 +0000

    commit

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -7,12 +7,12 @@ export const TICKETS_MOCKED: Ticket[] = [
     title: 'SI4 in Fès',
     description: '',
     date: dateToday,
-    author: 'Lachkar'
+    author: 'Paul'
   },
   {
     title: 'SI5 in El Hoceima',
     description: 'Description du voyage',
     date: dateToday,
-    author: 'El hassan'
+    author: 'Anakin'
   },
 ];
