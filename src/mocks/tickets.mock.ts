52e306064afb7d1056170c66dbfe3c400d2d0b3e
Lachkar El hassan <99615141+lachkar98@users.noreply.github.com>
2023-03-29 12:01:06 +0000

    change student in model

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -7,12 +7,12 @@ export const TICKETS_MOCKED: Ticket[] = [
     title: 'SI4 in Fès',
     description: '',
     date: dateToday,
-    author: 'Paul'
+    student: 'Paul'
   },
   {
     title: 'SI5 in El Hoceima',
     description: 'Description du voyage',
     date: dateToday,
-    author: 'Anakin'
+    student: 'Anakin'
   },
 ];
