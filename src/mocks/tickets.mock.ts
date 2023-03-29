bbfd3099632987eb8f150af5e8d3571f152241b0
Lachkar El hassan <99615141+lachkar98@users.noreply.github.com>
2023-03-29 12:14:59 +0000

    add major attribute

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -7,12 +7,14 @@ export const TICKETS_MOCKED: Ticket[] = [
     title: 'SI4 in Fès',
     description: '',
     date: dateToday,
-    student: 'Paul'
+    student: 'Paul',
+    major :'GB'
   },
   {
     title: 'SI5 in El Hoceima',
     description: 'Description du voyage',
     date: dateToday,
-    student: 'Anakin'
+    student: 'Anakin',
+    major :'SI'
   },
 ];
