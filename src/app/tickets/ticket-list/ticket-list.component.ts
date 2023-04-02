3879d40cd28f3e68709664aef2af2651785201b5
Lachkar El hassan <99615141+lachkar98@users.noreply.github.com>
2023-04-02 13:22:38 +0000

    ajouter des bouttons pour filtrer l'archivage

--- src/app/tickets/ticket-list/ticket-list.component.ts
+++ src/app/tickets/ticket-list/ticket-list.component.ts
@@ -10,8 +10,8 @@ import { Ticket } from '../../../models/ticket';
 export class TicketListComponent implements OnInit {
 
   public ticketList: Ticket[] = [];
-  public displayTicketArchived : boolean;
-  
+  public displayTicketArchived : boolean = true;
+
   constructor(public ticketService: TicketService) {
     this.ticketService.tickets$.subscribe((tickets) => this.ticketList = tickets);
   }
@@ -30,5 +30,15 @@ export class TicketListComponent implements OnInit {
 
   archiveTicket(ticket:Ticket){
     this.ticketService.archiveTicket(ticket);
+  }
+
+  hideArchivedTicket(){
+    this.displayTicketArchived = false;
+    console.log(this.displayTicketArchived);
+  }
+
+  showArchivedTicket(){
+    this.displayTicketArchived = true;
+    console.log(this.displayTicketArchived);
   }
 }
